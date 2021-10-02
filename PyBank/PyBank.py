7f2ea338e6bc2ac4274ebee8d1eb25072e3cfd74
sgodkhindi <godkhindis@gmail.com>
2021-10-02 15:22:12 -0400

    Python Challenge Homework Submission

---
+++ PyBank/PyBank.py
@@ -0,0 +1,108 @@
+# Modules
+import os
+import csv
+
+# Set path for file
+csvpath = os.path.join('PyBank', 'Resources', 'budget_data.csv')
+
+# Initialize Variables for Total Months, Total Profit & Loss, High Profit and Low Profit
+totalmonths = 0
+totalPNL = 0
+
+highprofit = 0
+lowprofit = 0
+
+currprofit = 0
+nextprofit = 0
+
+profitdiff1 = 0
+profitdiff2 = 0
+
+allPNL = []
+
+# Open the CSV
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    # Skip Header
+    csv_header = next(csvreader)
+    # Store the 1st Row
+    firstrow = next(csvreader)
+
+    currprofit = float(firstrow[1])
+    # Update Count of the Months and also Add the 1st Row's Profit/Loss into the total
+    totalmonths = totalmonths + 1
+    totalPNL = totalPNL + float(firstrow[1])
+
+    # Store the 2nd Row
+    nextrow = next(csvreader)
+    nextprofit = float(nextrow[1])
+    # Store the Month Name
+    currmonth = nextrow[0]
+    # Update Count of the Months and also Add the 1st Row's Profit/Loss into the total
+    totalmonths = totalmonths + 1
+    totalPNL = totalPNL + float(nextrow[1])
+    
+    # Compute Profit Difference Between 2nd Row and 1st Row
+    profitdiff1 = nextprofit - currprofit
+
+    # Append the Profit Difference to a List as an item for later Average
+    allPNL.append(profitdiff1)
+
+    # Store the Profit Difference to temporary Variables for later comparisons
+    highprofit = profitdiff1
+    highmonth = currmonth
+    
+    lowprofit = profitdiff1
+    lowmonth = currmonth
+   
+    # Assign the 2nd Rows Profit to a variable for comparison
+    currprofit = nextprofit
+    
+    # Read the file until End of File
+    for row in csvreader:
+        # Store the current row data into variables
+        nextprofit = float(row[1])
+        currmonth = row[0]
+
+        #Compute the Profit Difference between the current row and previous row
+        profitdiff2 = nextprofit - currprofit
+
+        # Update Count of the Months and also Add the 1st Row's Profit/Loss into the total     
+        totalmonths = totalmonths + 1
+        totalPNL = totalPNL + float(row[1])
+
+        # if Profit Difference between the current row and previous row is HIGHER than previous profit difference Store the details to HighProfit / HighMonth
+        if profitdiff2 > profitdiff1 and profitdiff2 > highprofit:
+            highprofit = profitdiff2
+            highmonth = currmonth
+           
+        # if Profit Difference between the current row and previous row is LOWER than previous profit difference Store the details to LowProfit / LowMonth    
+        elif profitdiff2 < profitdiff1 and profitdiff2 < lowprofit:
+            lowprofit = profitdiff2
+            lowmonth = currmonth
+        
+        # Save Current Row details before reading the next row
+        allPNL.append(profitdiff2)
+        currprofit = nextprofit
+        profitdiff1 = profitdiff2
+    
+    # Display the Computed Values on Screen
+    print ("Total: "+str(totalPNL))
+    print ("Total Months: "+ str(totalmonths))
+    print ("Greatest Increase in Profits: " + highmonth +" ($"+ str(round(highprofit,0))+ ")")
+    print ("Greatest Decrease in Profits: " + lowmonth +" ($"+ str(round(lowprofit,0))+ ")")
+    print ("Average Change: "+ str(round(sum(allPNL)/len(allPNL),2)))
+    
+    # Store the computed values into a text tfile
+    TextFile = open(r".\PyBank\Output\PyBank_Output.txt", "w")
+    
+    TextFile.write("Total: "+str(totalPNL))
+    TextFile.write("\n")
+    TextFile.write("Total Months: "+ str(totalmonths))
+    TextFile.write("\n")
+    TextFile.write("Greatest Increase in Profits: " + highmonth +" ($"+ str(round(highprofit,0))+ ")")
+    TextFile.write("\n")
+    TextFile.write("Greatest Decrease in Profits: " + lowmonth +" ($"+ str(round(lowprofit,0))+ ")")
+    TextFile.write("\n")
+    TextFile.write("Average Change: "+ str(round(sum(allPNL)/len(allPNL),2)))
+    TextFile.close()
---
+++ PyPoll/PyPoll.py
@@ -0,0 +1,98 @@
+# Modules
+import os
+import csv
+
+# Set path for file
+csvpath = os.path.join('PyPoll', 'Resources', 'election_data.csv')
+
+# Initialize Variables for Vote Counts for each candidate
+Khanvotes = 0
+Correyvotes = 0
+Livotes = 0
+OTooleyvotes = 0
+TotalVotes = 0
+
+# Initialize Variables for Vote Percentage for each candidate
+Khanvotes = 0
+KhanPercent = 0
+CorreyPercent = 0
+LiPercent = 0
+OTooleyPercent = 0
+
+# Open CSV file based on Path information in csvpath variable
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    # Skip the header
+    csv_header = next(csvreader)
+    
+    # Loop through file until End of File
+    for row in csvreader:
+        # Add to total Votes across all candidates
+        TotalVotes = TotalVotes + 1
+
+        # Check for Candidate Name to add to their individual totals
+
+        if (row[2] == "Khan"):
+            Khanvotes = Khanvotes + 1
+        elif (row[2] == "Correy"):
+            Correyvotes = Correyvotes + 1
+        elif (row[2] == "Li"):
+            Livotes = Livotes + 1
+        elif (row[2] == "O'Tooley"):
+            OTooleyvotes = OTooleyvotes + 1
+    
+    # Once File is completely read
+    # Compute Percentage for each Candidate
+    KhanPercent = Khanvotes/TotalVotes
+    CorreyPercent = Correyvotes/TotalVotes
+    LiPercent = Livotes/TotalVotes
+    OTooleyPercent = OTooleyvotes/TotalVotes
+
+    # Compare Individual Candidate Totals to Find out Overall Winner
+    if Khanvotes > Correyvotes and Khanvotes > Livotes and Khanvotes > OTooleyvotes:
+        Winner = "Khan"
+    elif Correyvotes > Khanvotes and Correyvotes > Livotes and Correyvotes > OTooleyvotes:
+        Winner = "Correy"
+    elif Livotes > Khanvotes and Livotes > Correyvotes and Livotes > OTooleyvotes:
+        Winner = "Li"
+    elif OTooleyvotes > Khanvotes and OTooleyvotes > Correyvotes and OTooleyvotes > Livotes:
+        Winner = "O'Tooley"
+    
+    # Display the Results of Total Votes and Each Individual Candidates' Percentage and Total Votes
+    print("Election Results")
+    print("-------------------------")
+    print("Total Votes: " + str(TotalVotes))
+    print("-------------------------")
+    print("Khan : " + str(round(KhanPercent*100,2)) +"%  ("+str(Khanvotes) +")")
+    print("Correy : " + str(round(CorreyPercent*100,2)) +"% (" + str(Correyvotes) +")")
+    print("Li : " + str(round(LiPercent*100,2)) +"%"+ " ("+str(Livotes) +")")
+    print("O'Tooley : " + str(round(OTooleyPercent*100,2)) +"%"+ " ("+str(OTooleyvotes) +")")
+    print("-------------------------")
+    print("Winner: " + Winner)
+    print("-------------------------")
+
+    # Write the Results of Total Votes and Each Individual Candidates' Percentage and Total Votes to a Text File
+    TextFile = open(r".\PyPoll\Output\PyPoll_Output.txt", "w")
+    
+    TextFile.write("Election Results")
+    TextFile.write("\n")
+    TextFile.write("-------------------------")
+    TextFile.write("\n")
+    TextFile.write("Total Votes: " + str(TotalVotes))
+    TextFile.write("\n")
+    TextFile.write("-------------------------")
+    TextFile.write("\n")
+    TextFile.write("Khan : " + str(round(KhanPercent*100,2)) +"%  ("+str(Khanvotes) +")")
+    TextFile.write("\n")
+    TextFile.write("Correy : " + str(round(CorreyPercent*100,2)) +"% (" + str(Correyvotes) +")")
+    TextFile.write("\n")
+    TextFile.write("Li : " + str(round(LiPercent*100,2)) +"%"+ " ("+str(Livotes) +")")
+    TextFile.write("\n")
+    TextFile.write("O'Tooley : " + str(round(OTooleyPercent*100,2)) +"%"+ " ("+str(OTooleyvotes) +")")
+    TextFile.write("\n")
+    TextFile.write("-------------------------")
+    TextFile.write("\n")
+    TextFile.write("Winner: " + Winner)
+    TextFile.write("\n")
+    TextFile.write("-------------------------")
+    TextFile.close()
